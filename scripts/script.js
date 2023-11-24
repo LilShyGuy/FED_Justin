5cb2ea5258eeb1aaaf67157d9761b05d52be5df0
LilShyGuy <121128850+LilShyGuy@users.noreply.github.com>
2023-11-24 11:27:26 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,9 @@
 // JavaScript Document
 console.log("hi");
+var menubutton = document.querySelector("button")
+var menuoptions = document.querySelector("#menuoptions")
+menubutton.addEventListener('click', mendropdown)
+function mendropdown(){
+    console.log("geklikt");
+    menuoptions.classList.toggle('hidden')
+}
